6e9c21a195af596dc1b19c95d4a4f930457ffed6
khbyun <novemberde@nate.com>
2018-01-23 20:28:53 +0900

    add all of binaryMimeTypes

--- src/handler.ts
+++ src/handler.ts
@@ -4,8 +4,24 @@ import * as sls from 'aws-serverless-express';
 
 const app: express.Application = new App().app;
 const binaryMimeTypes = [
-    "*/*"
+	'application/javascript',
+	'application/json',
+	'application/octet-stream',
+	'application/xml',
+	'font/eot',
+	'font/opentype',
+	'font/otf',
+	'image/jpeg',
+	'image/png',
+	'image/svg+xml',
+	'text/comma-separated-values',
+	'text/css',
+	'text/html',
+	'text/javascript',
+	'text/plain',
+	'text/text',
+	'text/xml'
 ];
 const server = sls.createServer(app, undefined, binaryMimeTypes);
 
-module.exports.api = (event: any, context: any) => sls.proxy(server, event, context);
+module.exports.app = (event: any, context: any) => sls.proxy(server, event, context);

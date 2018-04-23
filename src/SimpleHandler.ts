3fedad5e787df71da0c9e8b3a0b69b22753ec0a4
khbyun <novemberde@nate.com>
2018-04-23 14:56:51 +0900

    modify app on SimpleHandler

--- src/SimpleHandler.ts
+++ src/SimpleHandler.ts
@@ -1,5 +1,41 @@
-import { APIGatewayEvent, Context, Callback } from 'aws-lambda';
+import * as express from 'express';
+import * as sls from 'aws-serverless-express';
 
-export const app = (event: APIGatewayEvent, context: Context, callback: Callback) => {
-    callback(null, "Hello world from simple API");
-};
+class App {
+    public app: express.Application;
+
+    constructor () {
+        this.app = express();
+        this.app.get("/*", (req, res, next) => {
+            res.send("Hello from simple api");
+        });
+    }
+
+    public static bootstrap (): App {
+        return new App();
+    }
+}
+
+const app: express.Application = new App().app;
+const binaryMimeTypes = [
+	'application/javascript',
+	'application/json',
+	'application/octet-stream',
+	'application/xml',
+	'font/eot',
+	'font/opentype',
+	'font/otf',
+	'image/jpeg',
+	'image/png',
+	'image/svg+xml',
+	'text/comma-separated-values',
+	'text/css',
+	'text/html',
+	'text/javascript',
+	'text/plain',
+	'text/text',
+	'text/xml'
+];
+const server = sls.createServer(app, undefined, binaryMimeTypes);
+
+export const api = (event: any, context: any) => sls.proxy(server, event, context);

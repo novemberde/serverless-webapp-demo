69148d751ce8c29ab778e855c59ffbe91e11731b
khbyun <novemberde@nate.com>
2018-05-06 20:47:13 +0900

    Test rxjs on express

--- src/App.ts
+++ src/App.ts
@@ -1,18 +1,30 @@
 import * as express from 'express';
+import { Observable, Observer } from 'rxjs';
 
 class App {
     public app: express.Application;
+    public helloObservable: Observable<string>;
 
     constructor () {
         this.app = express();
-        this.app.get("/*", (req, res, next) => {
+        this.helloObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                observer.next("rxjs is working on express server!");
+            }, 3000);
+        });
+
+        this.app.get("/", (req, res, next) => {
             res.redirect("http://www.awskr.org/slack/");
+        });
+        this.app.get("/rx", (req, res, next) => {
+            this.helloObservable.subscribe(str => res.send(str));
+        });
+
+        this.app.use((req, res, next) => {
+            res.status(404).send("Not found");
         });
     }
 
-    public static bootstrap (): App {
-        return new App();
-    }
 }
 
 export default App;
